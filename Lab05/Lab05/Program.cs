8155408f1fb455b7167134e0d76535661144ecb5
Joe <Joe@DESKTOP-14EAMQK>
2019-11-25 09:00:32 -0500

    Pushing to move file to laptop

---
+++ Lab05/Lab05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // publici is the scope of the method , the method is non-static since it isn't specified as static, double is the return type, and getArea is the method name, return height * width is teh method body.
+            // 
+            //
+          
+
+        }
+    }
+}
--- Midterm/Midterm/Program.cs
+++ Midterm/Midterm/Program.cs
@@ -7,7 +7,44 @@ namespace Midterm
         static void Main(string[] args)
         {
             //
+            /*
+            bool keepLooping = true;
+
+            while (keepLooping == true) 
+            Console.WriteLine("true");
             
+
+
+            int i = 2;
+            {
+                    while (i < 128)
+                {
+                    Console.WriteLine("[" + i++ + "]");
+                    if (i == 65)
+                    {
+                        break;
+                    }
+
+                }
+            }
+           */
+             
+           
+
+            for (int e = 49; e >= 1; e--)
+            {
+                Console.Write(e);
+                {
+                    Console.Write(",");
+                    if (e == 2)
+                    {
+                        Console.Write("");
+                    }
+                }
+            }
+
+
+
 
         }
     }
