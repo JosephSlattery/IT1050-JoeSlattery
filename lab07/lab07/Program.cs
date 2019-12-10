512068eb0f2313a1225333ef3237534e0ec9a7bb
Joe <Joe@DESKTOP-14EAMQK>
2019-12-10 17:21:48 -0500

    LAB07 push

---
+++ lab07/lab07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LAB07
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            book book1 = new book("Moby Dick", " Herman Melville", 1956);
+
+
+        }
+    }
+    class book
+    {
+        public string title;
+        public string auther;
+        public int year;
+
+        public book(string title, string auther, int year)
+        {
+            this.title = title;
+            this.auther = auther;
+            this.year = year;
+        }
+
+        void display()
+        {
+            Console.WriteLine(
+                $"Title: {this.title}\n" +
+                $"auther: {this.auther}\n" +
+                $"year: {this.year}\n:");
+        }
+    }
+
+}
